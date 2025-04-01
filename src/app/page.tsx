c56c94fd5776264b19c46eb414929ea62c461d0c
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-04-01 12:24:18 +0900

    💄 リンク追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,33 @@ import { Tabs } from '../components/ui'
 
 const Home = () => {
   return (
-    <div className='max-w-screen-md mx-auto flex flex-col items-center my-6'>
-    <Tabs aria-label="Recipe App" className="w-full">
-      <Tabs.List className={'border-b'}>
-        <Tabs.Tab id="r">Recipes</Tabs.Tab>
-        <Tabs.Tab id="i">Ingredients</Tabs.Tab>
-        <Tabs.Tab id="m">Meal Plans</Tabs.Tab>
-        <Tabs.Tab id="v">Videos</Tabs.Tab>
-      </Tabs.List>
-      <Tabs.Panel id="r">
-        Browse through a wide selection of recipes for all occasions and dietary
-        preferences.
-      </Tabs.Panel>
-      <Tabs.Panel id="i">
-        Check the list of ingredients needed for your chosen recipes.
-      </Tabs.Panel>
-      <Tabs.Panel id="m">
-        Discover curated meal plans to simplify your weekly cooking.
-      </Tabs.Panel>
-      <Tabs.Panel id="v">
-        Watch cooking videos to learn new techniques and recipes.
-      </Tabs.Panel>
-    </Tabs>
+    <div className="max-w-screen-md mx-auto flex flex-col items-center my-6">
+      <Tabs aria-label="Recipe App" className="w-full">
+        <Tabs.List className={'border-b'}>
+          <Tabs.Tab id="r">Authentication</Tabs.Tab>
+          <Tabs.Tab id="i">Ingredients</Tabs.Tab>
+          <Tabs.Tab id="m">Meal Plans</Tabs.Tab>
+          <Tabs.Tab id="v">Videos</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel id="r">
+          <a
+            target="_blank"
+            href="https://my-first-auth0.vercel.app/auth/login"
+            rel="noreferrer"
+          >
+            login
+          </a>
+        </Tabs.Panel>
+        <Tabs.Panel id="i">
+          Check the list of ingredients needed for your chosen recipes.
+        </Tabs.Panel>
+        <Tabs.Panel id="m">
+          Discover curated meal plans to simplify your weekly cooking.
+        </Tabs.Panel>
+        <Tabs.Panel id="v">
+          Watch cooking videos to learn new techniques and recipes.
+        </Tabs.Panel>
+      </Tabs>
     </div>
   )
 }
--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
 
 const ShareButton = dynamic(
   () =>
@@ -15,6 +16,14 @@ const SharePage = () => {
     <div className="max-w-screen-md mx-auto flex flex-col items-center my-6 gap-4">
       <h1 className="text-2xl font-bold">share page</h1>
       <ShareButton />
+      <Link href={'/'}>
+        <button
+          type="button"
+          className="bg-slate-700 px-4 py-2 rounded-md text-white shadow-md"
+        >
+          button
+        </button>
+      </Link>
     </div>
   )
 }
